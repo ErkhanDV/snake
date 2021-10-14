4aa49f5f76f8a8d7dd9cbc6120a3b699cff67a82
ErkhanDV <erhan.denis@list.ru>
2021-10-14 23:58:52 +0500

    create classes to draw playing field #3

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,61 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-class App extends React.Component {
+class Square extends React.Component {
   render() {
-    return React.createElement("div", null, React.createElement("h1", null, "Let's play!"));
+    return React.createElement("div", {
+      className: `square-${this.props.data.type}`
+    });
+  }
+
+}
+
+class Row extends React.Component {
+  render() {
+    return React.createElement("div", {
+      className: "row"
+    }, this.props.data.map((squareData, index) => React.createElement(Square, {
+      data: squareData,
+      key: index
+    })));
+  }
+
+}
+
+class Field extends React.Component {
+  render() {
+    return React.createElement("div", {
+      className: "field"
+    }, this.props.data.map((rowData, index) => React.createElement(Row, {
+      data: rowData,
+      key: index
+    })));
+  }
+
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    let fieldData = [];
+
+    for (let i = 0; i < 10; i++) {
+      fieldData.push([]);
+
+      for (let j = 0; j < 10; j++) {
+        fieldData[i].push({
+          type: 'empty'
+        });
+      }
+    }
+
+    this.state = {
+      fieldData: fieldData
+    };
+  }
+
+  render() {
+    return React.createElement("div", null, React.createElement("h1", null, "Let's play!"), React.createElement(Field, {
+      data: this.state.fieldData
+    }));
   }
 
 }
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,8 +1,55 @@
+class Square extends React.Component {
+
+  render() {
+    return(
+      <div className= {`square-${this.props.data.type}`}></div>
+    );
+  }
+}
+
+class Row extends React.Component {
+
+  render() {
+    return(
+      <div className="row">
+        {this.props.data.map((squareData, index) => <Square data={squareData} key={index}/>)}
+      </div>
+    );
+  }
+}
+
+class Field extends React.Component {
+
+  render() {
+    return(
+      <div className="field">
+        {this.props.data.map((rowData, index) => <Row data={rowData} key={index}/>)}
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    let fieldData = [];
+    for (let i = 0; i < 10; i++) {
+      fieldData.push([]);
+      for (let j = 0; j < 10; j++) {
+        fieldData[i].push({type: 'empty'});
+      }
+    }
+    this.state = {
+      fieldData: fieldData
+    };
+  }
+
   render() {
     return(
       <div>
         <h1>Let's play!</h1>
+        <Field data={this.state.fieldData} />
       </div>
     );
   }
